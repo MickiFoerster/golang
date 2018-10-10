42990b418ec0bcdb368d3feb7a15e210c99a17ef
Michael Foerster <Michael.Foerster@com-science.de>
2018-10-10 22:05:12 +0200

    new examples

---
+++ pipeline-example-idiomatic.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for x := 0; x<10; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for square := range squares {
+		fmt.Println(square)
+	}
+}
