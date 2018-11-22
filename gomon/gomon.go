209ab05bdcdc3d711b40c8e56e84cf0eb65d837d
Michael Foerster <Michael.Foerster@com-science.de>
2018-11-22 15:54:46 +0100

    Add not working version of gomon

--- gomon/gomon.go
+++ gomon/gomon.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path"
+	"strings"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c)
+	go func() {
+		for {
+			sign := <-c
+			fmt.Println("got signal:", sign)
+		}
+	}()
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -22,13 +35,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	GoFile := os.Args[1]
-	cmd := exec.Command("go", "run", GoFile)
-	fmt.Println("execute", cmd)
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
+	if !strings.HasSuffix(path.Ext(os.Args[1]), ".go") {
+		fmt.Fprintf(os.Stderr, "The extension of %q is not '.go'\n", os.Args[1])
 	}
+
+	build(os.Args[1])
 
 	done := make(chan struct{})
 	go func() {
@@ -39,9 +50,12 @@ func main() {
 					return
 				}
 				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
+				err = watcher.Add(os.Args[1])
+				if err != nil {
+					log.Fatal(err)
 				}
+				fmt.Println("Restart Go file", executeableName)
+				go startCmd()
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
@@ -51,9 +65,53 @@ func main() {
 		}
 	}()
 
-	err = watcher.Add(GoFile)
+	err = watcher.Add(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
+	go startCmd()
 	<-done
+}
+
+var cmd *exec.Cmd
+var executeableName string
+
+func build(goFile string) {
+	buildcmd := exec.Command("go", "build", goFile)
+	err := buildcmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	executeableName = path.Join(pwd, goFile[:len(goFile)-len(".go")])
+	fmt.Println(executeableName)
+}
+
+func startCmd() {
+	if cmd != nil {
+		err := cmd.Process.Kill()
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Second)
+	}
+	cmd = exec.Command(executeableName)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	go io.Copy(os.Stdout, stdout)
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Command %q started with PID %d\n", executeableName, cmd.Process.Pid)
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Command finished")
 }
