4d11cc85752d608445fe64983b552db3a8356b30
Michael Foerster <Michael.Foerster@com-science.de>
2018-12-08 14:56:27 +0100

    update

--- net/tls/main.go
+++ net/tls/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
 func main() {
-	redirectHTTP()
 	http.HandleFunc("/", foo)
 	http.ListenAndServeTLS(":4443", "cert.pem", "key.pem", nil)
 }
@@ -14,19 +12,4 @@ func main() {
 func foo(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("This is an example server.\n"))
-}
-
-func redirectHTTP() {
-	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
-}
-
-func redirect(w http.ResponseWriter, req *http.Request) {
-	target := "https://" + req.Host + req.URL.Path
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
-	}
-	log.Printf("redirect to: %s", target)
-	http.Redirect(w, req, target,
-		// see @andreiavrammsd comment: often 307 > 301
-		http.StatusTemporaryRedirect)
 }
