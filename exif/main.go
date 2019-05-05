3ba1d72a8daa31f59a9d0eddfd0e49aa0c60ddf9
Michael Foerster <Michael.Foerster@com-science.de>
2019-05-05 23:12:19 +0200

    Recursive search for files with EXIF data

--- exif/main.go
+++ exif/main.go
@@ -1,18 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
 )
 
-func main() {
+var filelist = make([]string, 0, 1024)
+
+func init() {
 	if len(os.Args) < 2 {
-		log.Fatalf("syntax error: %s <path to JPG file>\n", os.Args[0])
+		log.Fatalf("syntax error: %s <path where files with EXIF data are located>\n", os.Args[0])
+	}
+	dir := os.Args[1]
+
+	if info, err := os.Stat(dir); os.IsNotExist(err) || !info.IsDir() {
+		log.Fatal("error: `", dir, "` is not a valid path to a directory.")
+	}
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		log.Fatal("error: Path `", dir, "` does not exist:", err)
 	}
 
-	f, err := os.Open(os.Args[1])
+	err := filepath.Walk(dir, walker)
 	if err != nil {
-		log.Fatal("error: Opening file", os.Args[1], "failed")
+		log.Fatal("error: Cannot read list of files:", err)
 	}
-	f.Close()
+}
+
+func main() {
+	for _, fn := range filelist {
+		handleFile(fn)
+	}
+}
+
+func handleFile(fn string) {
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Fatal("error: Opening file", fn, "failed:", err)
+	}
+	defer f.Close()
+
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		return
+	}
+	tm, _ := x.DateTime()
+	fmt.Println("Taken: ", tm)
+
+	lat, long, err := x.LatLong()
+	if err == nil {
+		fmt.Println("lat, long: ", lat, ", ", long)
+	}
+}
+
+func walker(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		fmt.Println("error:", err)
+		return err
+	}
+	if !info.IsDir() {
+		filelist = append(filelist, path)
+	}
+	return nil
 }
