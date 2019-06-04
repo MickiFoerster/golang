433bf54750083c29afd7e9551090aa7613f9b561
Michael Foerster <Michael.Foerster@com-science.de>
2019-06-04 23:59:09 +0200

    Update gogrep example

--- gogrep.go
+++ gogrep.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"sync"
 )
 
 const numOfConcurrentWorker = 8
@@ -14,6 +15,7 @@ const numOfConcurrentWorker = 8
 var (
 	filelist = make(chan string)
 	done     = make(chan struct{})
+	wg       sync.WaitGroup
 )
 
 func main() {
@@ -21,6 +23,7 @@ func main() {
 		concurrentSemaphore := make(chan struct{}, 32)
 		for fn := range filelist {
 			concurrentSemaphore <- struct{}{} // down
+			wg.Add(1)
 			go func(path string) {
 				err := grep(path)
 				if err != nil {
@@ -31,6 +34,7 @@ func main() {
 		}
 		done <- struct{}{}
 	}()
+
 	go func() {
 		err := filepath.Walk(".", walker)
 		if err != nil {
@@ -38,7 +42,9 @@ func main() {
 		}
 		close(filelist)
 	}()
+
 	<-done
+	wg.Wait()
 }
 
 func walker(path string, info os.FileInfo, err error) error {
@@ -56,24 +62,57 @@ func grep(fn string) error {
 	if len(os.Args) != 2 {
 		return fmt.Errorf("syntax error: Give pattern to search for as first argument")
 	}
+	pattern := fmt.Sprintf(`.*%s.*`, os.Args[1])
+	patternlen := len(os.Args[1])
+	regExpr := regexp.MustCompile(pattern)
 
 	f, err := os.Open(fn)
 	if err != nil {
 		return err
 	}
-	pattern := fmt.Sprintf(`.*%s.*`, os.Args[1])
-	regExpr := regexp.MustCompile(pattern)
+	defer f.Close()
+
 	scanner := bufio.NewScanner(f)
-	counter := 1
+	scanner.Split(bufio.ScanBytes)
+
+	linecounter := 1
+	counter := 0
+	buf := make([]byte, 128)
+
 	for scanner.Scan() {
-		if matched := regExpr.Match([]byte(scanner.Text())); matched {
-			fmt.Printf("%s:%d: %s\n", fn, counter, scanner.Text())
+		bytes := scanner.Bytes()
+		for _, b := range bytes {
+			if b == '\n' {
+				if matched := regExpr.Match(buf[:counter]); matched {
+					fmt.Printf("%s:%d: %s\n", fn, linecounter, string(buf))
+				}
+				linecounter++
+				counter = 0
+			}
+			if counter+1 == len(buf) {
+				if matched := regExpr.Match(buf); matched {
+					fmt.Printf("%s:%d: %s\n", fn, linecounter, string(buf))
+				}
+
+				for i := 0; i < patternlen; i++ {
+					buf[i] = buf[len(buf)-patternlen+i]
+				}
+				counter = patternlen
+			}
+			buf[counter] = b
+			counter++
 		}
-		counter++
 	}
 	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("error: read operation failed:", err)
+		return fmt.Errorf("error: read operation failed: %s", err)
 	}
+
+	if counter > 0 {
+		if matched := regExpr.Match(buf[:counter]); matched {
+			fmt.Printf("%s:%d: %s\n", fn, linecounter, string(buf))
+		}
+	}
+	wg.Done()
 
 	return nil
 }
