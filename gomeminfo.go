5f47f71c8dbe1e4a647fc896feec083b5314176c
Michael Foerster <Michael.Foerster@com-science.de>
2019-06-22 00:33:02 +0200

    Add gomeminfo example

--- gomeminfo.go
+++ gomeminfo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,15 +17,48 @@ func main() {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+	var totalKB float64
+	var availKB float64
+loop:
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) > 0 {
-			match := strings.HasPrefix(line, "MemTotal:") ||
-				strings.HasPrefix(line, "MemFree:") ||
-				strings.HasPrefix(line, "MemAvailable:")
-			if match {
-				fmt.Println(line)
+			total := false
+			free := false
+			avail := false
+			switch {
+			case strings.HasPrefix(line, "MemTotal:"):
+				line = line[len("MemTotal: ") : len(line)-3]
+				total = true
+			case strings.HasPrefix(line, "MemFree:"):
+				line = line[len("MemFree: ") : len(line)-3]
+				free = true
+			case strings.HasPrefix(line, "MemAvailable:"):
+				line = line[len("MemAvailable: ") : len(line)-3]
+				avail = true
+			default:
+				break loop
+			}
+
+			line = strings.Trim(line, " ")
+			val, err := strconv.ParseUint(line, 10, 64)
+			if err != nil {
+				fmt.Println("Parse error: ", line, err)
+				continue
+			}
+
+			switch {
+			case total:
+				totalKB = float64(val)
+			case free:
+				_ = val
+			case avail:
+				availKB = float64(val)
+			default:
+				log.Fatal("unexpected error")
 			}
 		}
 	}
+	memUsed := totalKB - availKB
+	fmt.Printf("mem usage: %.2f%%\n", memUsed*100/totalKB)
 }
