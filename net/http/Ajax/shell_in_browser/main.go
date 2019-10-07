e6e18f1c0f464b1e035d5a9844f44fcc60ed08ff
micki <github@com-science.de>
2019-10-07 22:12:19 +0200

    Add Ajax example where shell command is executed

--- net/http/Ajax/shell_in_browser/main.go
+++ net/http/Ajax/shell_in_browser/main.go
@@ -20,20 +20,37 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/shell", shell)
+	http.HandleFunc("/checkIfCommandExist", checkIfCommandExist)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-		host := req.FormValue("host")
-		port := req.FormValue("port")
-		fmt.Println(w, `<html>`)
-		fmt.Println(w, `<body>`)
-		fmt.Fprintf(w, `<pre>%s</pre>`, host)
-		fmt.Fprintf(w, `<pre>%s</pre>`, port)
-		fmt.Println(w, `</body>`)
-		fmt.Println(w, `</html>`)
+		/*
+			fmt.Println(`method is POST`)
+			host := req.FormValue("host")
+			port := req.FormValue("port")
+			fmt.Fprintln(w, `<html>`)
+			fmt.Fprintln(w, `<body>`)
+			fmt.Fprintf(w, `<pre>%s</pre>`, host)
+			fmt.Fprintf(w, `<pre>%s</pre>`, port)
+			fmt.Fprintln(w, `</body>`)
+			fmt.Fprintln(w, `</html>`)
+		*/
+		http.Redirect(w, req, "/shell", http.StatusSeeOther)
 		return
 	}
-	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+	fmt.Println(`method is GET`)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func checkIfCommandExist(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func shell(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "shell.gohtml", nil)
 }
