3ed01543f53e4bffde96c33d57a2428ace7d5265
Michael Foerster <Michael.Foerster@com-science.de>
2019-10-07 22:39:41 +0200

    Execute 'which' on server and send answer to client

--- net/http/Ajax/shell_in_browser/main.go
+++ net/http/Ajax/shell_in_browser/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -29,17 +33,6 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-		/*
-			fmt.Println(`method is POST`)
-			host := req.FormValue("host")
-			port := req.FormValue("port")
-			fmt.Fprintln(w, `<html>`)
-			fmt.Fprintln(w, `<body>`)
-			fmt.Fprintf(w, `<pre>%s</pre>`, host)
-			fmt.Fprintf(w, `<pre>%s</pre>`, port)
-			fmt.Fprintln(w, `</body>`)
-			fmt.Fprintln(w, `</html>`)
-		*/
 		http.Redirect(w, req, "/shell", http.StatusSeeOther)
 		return
 	}
@@ -48,7 +41,18 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func checkIfCommandExist(w http.ResponseWriter, req *http.Request) {
+	cmd, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read body: %v\n", err)
+	}
 
+	which, err := exec.Command("which", string(cmd)).Output()
+	if err != nil {
+		fmt.Fprintf(w, "")
+	}
+
+	path := strings.TrimRight(string(which), " \r\n")
+	fmt.Fprintf(w, path)
 }
 
 func shell(w http.ResponseWriter, req *http.Request) {
