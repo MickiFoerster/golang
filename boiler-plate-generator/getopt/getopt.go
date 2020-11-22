8e1784d6a1e4c69835a7a92c9e0f7f4ff18d0655
micki <github@com-science.de>
2020-11-22 21:48:00 +0100

    getoptions: Read YAML file with definition of options

--- boiler-plate-generator/getopt/getopt.go
+++ boiler-plate-generator/getopt/getopt.go
@@ -8,19 +8,22 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type Option struct {
-	Name         string `yaml:"name"`
-	Abbreviation string `yaml:"abbreviation"`
-	HasArg       string `yaml:"has_arg"`
-}
-
-type Options struct {
-	Options Option `yaml:"option"`
+type Options []struct {
+	Option struct {
+		Name         string `yaml:"name"`
+		Abbreviation string `yaml:"abbreviation"`
+		HasArg       struct {
+			Type             string `yaml:"type"`
+			OptionalArgument struct {
+				Type   string   `yaml:"type"`
+				Values []string `yaml:"values"`
+			} `yaml:"optional_argument"`
+		} `yaml:"has_arg"`
+	} `yaml:"option"`
 }
 
 func main() {
-	//const fn = "getoptions.yaml"
-	const fn = "a.yaml"
+	const fn = "getoptions.yaml"
 	data, err := ioutil.ReadFile(fn)
 	if err != nil {
 		log.Fatalf("error: could not read file: %v", err)
