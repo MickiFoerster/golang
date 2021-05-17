981932436c4eb3404153e1cb0622d384aed84891
micki <github@com-science.de>
2021-05-17 22:14:00 +0200

    Add 3rd command-line argument representing port

--- net/http/FileServer/main.go
+++ net/http/FileServer/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalf("syntax error: %s <local folder to serve via http>", os.Args[0])
+	if len(os.Args) != 3 {
+		log.Fatalf("syntax error: %s <local folder to serve via http> <port>", os.Args[0])
 	}
 
 	dir := os.Args[1]
+	port := fmt.Sprintf(":%s", os.Args[2])
 	if stat, err := os.Stat(dir); os.IsNotExist(err) {
 		log.Fatalf("directory %s does not exist", dir)
 	} else if !stat.IsDir() {
 		log.Fatalf("%s is not a directory", dir)
 	}
 	http.Handle("/", http.FileServer(http.Dir(dir)))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
