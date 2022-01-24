caa725678c5dfcc3bc703926575ad98aaa82465e
micki <github@com-science.de>
2022-01-24 17:38:25 +0100

    postgres: Add ping example to postgres database

---
+++ postgres/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://user1:password@localhost/mydatabase?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection to Postgres database successful.")
+}
